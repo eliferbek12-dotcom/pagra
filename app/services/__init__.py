ba4a86560fb170ce65bf338ed1cf8b66ffcf75d7
Elif Erbek <elif.erbek17@gmail.com>
2026-08-20 16:32:27 +0300

    services init dosyasi temizlendi

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,10 +4,6 @@ from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    
-    # Tüm route'lara dışarıdan (Wix dahil) erişim izni ver:
     CORS(app, resources={r"/*": {"origins": "*"}})
-    
     app.register_blueprint(main)
-    
     return app
