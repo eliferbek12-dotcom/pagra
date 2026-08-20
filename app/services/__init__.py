80a3ad63ddb67218bea6209d8edf357c7eb76d73
Elif Erbek <elif.erbek17@gmail.com>
2026-08-20 17:08:00 +0300

    services init dosyasi sifirlandi

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,1 @@
-from flask import Flask
-from flask_cors import CORS
-from .routes import main
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    app.register_blueprint(main)
-    return app
